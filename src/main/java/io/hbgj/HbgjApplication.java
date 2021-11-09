1182426fff1932afa0e25c79c5a4237409be2f17
ftd_crazy@163.com <ftd_crazy@163.com>
2021-11-09 10:04:27 +0800

    提交成功

---
+++ src/main/java/io/hbgj/HbgjApplication.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2016-2019  All rights reserved.
+ *
+ * https://www.hbgj.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.hbgj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class HbgjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbgjApplication.class, args);
+	}
+
+}
